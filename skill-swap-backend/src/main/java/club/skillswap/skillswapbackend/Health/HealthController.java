c26b98923675ddb622d37183f2c1f7e5ace6e94f
LiChenyang-GZ <1169281314@qq.com>
2026-03-02 10:56:46 +1100

    log health hit

--- skill-swap-backend/src/main/java/club/skillswap/skillswapbackend/Health/HealthController.java
+++ skill-swap-backend/src/main/java/club/skillswap/skillswapbackend/Health/HealthController.java
@@ -1,14 +1,18 @@
 package club.skillswap.skillswapbackend.Health;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HealthController {
 
     @GetMapping("/health")
     public ResponseEntity<String> health() {
-        return ResponseEntity.ok("ok");
+        log.info("HEALTH HIT");
+        // return ResponseEntity.ok("ok");
+        return ResponseEntity.noContent().build();
     }
 }
